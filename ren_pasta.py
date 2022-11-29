18a71dd375ed756c331cb95528b10e49227ebb77
Vitor-Life <mailto:vitor@lifecti.com.br>
2022-11-29 10:50:12 -0300

    add diretorio

--- ren_pasta.py
+++ ren_pasta.py
@@ -1,12 +1,16 @@
 import os
 
-def main ():
+def principal ():
     i = 0
-    path =  "C:/Users/Vitor Life/Pictures/Saved Pictures/"
-    for filename in os.listdir(path):
-        my_dest = "imagem" + str(i) + ".jpg"
-
+             #coloque aqui o diretorio que deseja renomear
+    diretorio = "C:/Users/Vitor Life/Pictures/Saved Pictures/"
+    for filename in os.listdir(diretorio):
+        destino = "img" + str(i) + ".jpg"
+        origem = diretorio + filename
+        destino = diretorio + destino
+        os.rename(origem, destino)
+        i += 1
 
 if __name__ == '__main__':
-    main()
+    principal()
 
